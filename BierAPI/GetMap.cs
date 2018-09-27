817448a456e06b866cb197005efd4e4f1e651477
umeer arshad <umeer.arshad@hotmail.com>
2018-09-27 16:11:06 +0200

    dadsadadasdasdasda

--- BierAPI/GetMap.cs
+++ BierAPI/GetMap.cs
@@ -22,103 +22,112 @@ namespace BierAPI
         [FunctionName("GetMap")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
-            //Get streetname from query parameter
-            string city = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "city", true) == 0)
-                .Value;
-
-            if (city == null)
+            try
             {
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                city = data?.city;
-            }
+                //Get streetname from query parameter
+                string city = req.GetQueryNameValuePairs()
+                    .FirstOrDefault(q => string.Compare(q.Key, "city", true) == 0)
+                    .Value;
 
-            //Get country from query parameter
-            string country = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "country", true) == 0)
-                .Value;
-
-            if (country == null)
-            {
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                country = data?.country;
-            }
-
-            //Als er wel een stad en land is ingevuld
-            if(country != null && city != null)
-            {
-                //Google api key ophalen en API url opstellen om te checken of de combinatie bestaat en de long en lat op te halen.
-                string googleapikey = Environment.GetEnvironmentVariable("GoogleAPIkey");
-                var url = String.Format("https://maps.googleapis.com/maps/api/geocode/json?address={0},+{1}&key={2}", city, country, googleapikey);
-
-                using (var client = new HttpClient())
+                if (city == null)
                 {
-                    //Wachten op een response
-                    client.BaseAddress = new Uri(url);
-                    HttpResponseMessage response = await client.GetAsync(url);
+                    dynamic data = await req.Content.ReadAsAsync<object>();
+                    city = data?.city;
+                }
 
-                    //Als de response een sucess was
-                    if(response.IsSuccessStatusCode)
+                //Get country from query parameter
+                string country = req.GetQueryNameValuePairs()
+                    .FirstOrDefault(q => string.Compare(q.Key, "country", true) == 0)
+                    .Value;
+
+                if (country == null)
+                {
+                    dynamic data = await req.Content.ReadAsAsync<object>();
+                    country = data?.country;
+                }
+
+                //Als er wel een stad en land is ingevuld
+                if (country != null && city != null)
+                {
+                    //Google api key ophalen en API url opstellen om te checken of de combinatie bestaat en de long en lat op te halen.
+                    string googleapikey = Environment.GetEnvironmentVariable("GoogleAPIkey");
+                    var url = String.Format("https://maps.googleapis.com/maps/api/geocode/json?address={0},+{1}&key={2}", city, country, googleapikey);
+                    log.Info("Google api link: " + url);
+                    using (var client = new HttpClient())
                     {
-                        //String ophalen en omzetten naar een json object
-                        string strResult = await response.Content.ReadAsStringAsync();
-                        dynamic googlemapsobject = JsonConvert.DeserializeObject<dynamic>(strResult);
+                        //Wachten op een response
+                        client.BaseAddress = new Uri(url);
+                        HttpResponseMessage response = await client.GetAsync(url);
 
-                        //De status uit het json object lezen
-                        string status = (string)googlemapsobject.status;
-                        
-                        //Als er geen resultaten waren voor de opgegeven combinatie
-                        if(status == "ZERO_RESULTS")
+                        //Als de response een sucess was
+                        if (response.IsSuccessStatusCode)
                         {
-                            return req.CreateErrorResponse(HttpStatusCode.NotFound, "Your combination of city and country could not be found, please enter a valid city and country!");
-                        }
+                            //String ophalen en omzetten naar een json object
+                            string strResult = await response.Content.ReadAsStringAsync();
+                            dynamic googlemapsobject = JsonConvert.DeserializeObject<dynamic>(strResult);
 
-                        //Als er wel resultaten waren
-                        else
-                        {
-                            //Geocoordinaten ophalen voor de locatie
-                            string latitude = googlemapsobject.results[0].geometry.location.lat;
-                            string longtitude = googlemapsobject.results[0].geometry.location.lng;
+                            //De status uit het json object lezen
+                            string status = (string)googlemapsobject.status;
 
-                            //Bloblocatie opstellen
-                            string blobname = String.Format("Generatedmap-{0},{1}-{2}.png", city, country, DateTime.Now.ToFileTime());
-                            string blobcontainerreference = "mapblob";
-                            CloudStorageAccount account = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("StorageConnectionKey"));
-                            string blobUrl = account.BlobStorageUri.PrimaryUri.AbsoluteUri + "/" + blobcontainerreference + "/" + blobname;
-                            
-                            //Object voor het doorsturen naar de queue aanmaken
-                            QueueStorageMessage message = new QueueStorageMessage(longtitude, latitude, blobname, blobcontainerreference);
-
-                            //Als json string doorsturen naar de queue storage
-                            string json = JsonConvert.SerializeObject(message);
-                            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("StorageConnectionKey"));
-                            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
-                            CloudQueue queue = queueClient.GetQueueReference("bierapi-queue");
-                            queue.CreateIfNotExists();
-                            CloudQueueMessage queueumessage = new CloudQueueMessage(json);
-                            queue.AddMessage(queueumessage);
-
-                            var myObj = new { status = "request made", url = blobUrl, message = "Your requested map will be available at the url shortly"};
-                            var jsonToReturn = JsonConvert.SerializeObject(myObj);
-
-                            return new HttpResponseMessage(HttpStatusCode.OK)
+                            //Als er geen resultaten waren voor de opgegeven combinatie
+                            if (status == "ZERO_RESULTS")
                             {
-                                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
-                            };
+                                return req.CreateErrorResponse(HttpStatusCode.NotFound, "Your combination of city and country could not be found, please enter a valid city and country!");
+                            }
+
+                            //Als er wel resultaten waren
+                            else
+                            {
+                                //Geocoordinaten ophalen voor de locatie
+                                string latitude = googlemapsobject.results[0].geometry.location.lat;
+                                string longtitude = googlemapsobject.results[0].geometry.location.lng;
+
+                                //Bloblocatie opstellen
+                                string blobname = String.Format("Generatedmap-{0},{1}-{2}.png", city, country, DateTime.Now.ToFileTime());
+                                string blobcontainerreference = "mapblob";
+                                CloudStorageAccount account = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("StorageConnectionKey"));
+                                string blobUrl = account.BlobStorageUri.PrimaryUri.AbsoluteUri + "/" + blobcontainerreference + "/" + blobname;
+                                log.Info("bloburl= " + blobUrl);
+
+                                //Object voor het doorsturen naar de queue aanmaken
+                                QueueStorageMessage message = new QueueStorageMessage(longtitude, latitude, blobname, blobcontainerreference);
+
+                                //Als json string doorsturen naar de queue storage
+                                string json = JsonConvert.SerializeObject(message);
+                                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("StorageConnectionKey"));
+                                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+                                CloudQueue queue = queueClient.GetQueueReference("bierapi-queue");
+                                queue.CreateIfNotExists();
+                                CloudQueueMessage queueumessage = new CloudQueueMessage(json);
+                                queue.AddMessage(queueumessage);
+
+                                var myObj = new { status = "request made", url = blobUrl, message = "Your requested map will be available at the url shortly" };
+                                var jsonToReturn = JsonConvert.SerializeObject(myObj);
+
+                                return new HttpResponseMessage(HttpStatusCode.OK)
+                                {
+                                    Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+                                };
+                            }
                         }
                     }
                 }
+
+                else
+                {
+                    var myObj = new { status = "ERROR", message = "Please use a valid country name and city name!" };
+                    var jsonToReturn = JsonConvert.SerializeObject(myObj);
+
+                    return new HttpResponseMessage(HttpStatusCode.BadRequest)
+                    {
+                        Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+                    };
+                }
             }
 
-            else
+            catch(Exception ex)
             {
-                var myObj = new { status = "ERROR", message = "Please use a valid country name and city name!" };
-                var jsonToReturn = JsonConvert.SerializeObject(myObj);
-
-                return new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
-                };
+                log.Info("Exception" + ex.Message + " " + ex.InnerException);
             }
 
             return null;
